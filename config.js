0a7a40e632e47c37aae685bc18eb1adfd49f7622
rominarea <rominarea@gmail.com>
2023-04-24 22:27:37 -0300

    agregado json  y localstorage

--- config.js
+++ config.js
@@ -1,172 +1,158 @@
-// Bienvenida al sistema
-let nombre = prompt("Por favor, ingresa tu nombre:");
-alert("¡Bienvenido/a " + nombre + "! Comencemos a planificar tu inscripción");
+// BIENVENIDA AL SISTEMA
 
-class Materia{
-    //Creacion de materias
-    constructor(nombre, codigo, horario){
-        this.nombre = nombre;
-        this.codigo = codigo;
-        this.horario = horario;
+//Bienvenida al usuario
+document.getElementById("btn-bienvenida").addEventListener("click", function () {
+    let nombreUsuario = document.getElementById("nombre").value;
+    let mensaje = document.getElementById("mensaje");
+    mensaje.innerText = "Bienvenido/a " + nombreUsuario;
+});
+
+// Selección de elementos del DOM
+const selectorMateria1 = document.getElementById('materia1');
+const selectorMateria2 = document.getElementById('materia2');
+const selectorMateria3 = document.getElementById('materia3');
+const inputNota1 = document.getElementById('nota1');
+const inputNota2 = document.getElementById('nota2');
+const inputNota3 = document.getElementById('nota3');
+const formMaterias = document.getElementById('form-materias');
+const formCursada = document.getElementById('form-cursada');
+const resultados = document.getElementById('resultados');
+const promedio = document.getElementById('promedio');
+const mensajePromedio = document.getElementById('mensaje-promedio');
+const materiaRecomendada = document.getElementById('materia-recomendada');
+
+// Evento "change" en cada selector de materia
+selectorMateria1.addEventListener('change', (event) => {
+    if (event.target.value === "si") {
+        inputNota1.disabled = false;
+    } else {
+        inputNota1.disabled = true;
+        inputNota1.value = '';
     }
-    // Función para preguntar por una materia y su nota
-    preguntarPorMateria() {
-        let respuesta = prompt("Cursaste " + this.nombre + "? si/no");
-        if (respuesta === "si") {
-            let notaMateria = parseInt(prompt("Ingresá tu nota en la materia:"));
-            return {
-                aprobado: validarNota(notaMateria),
-                nota: notaMateria
-            };
-        } else {
-            alert("No cursaste " + this.nombre);
-            return {
-                aprobado: false,
-                nota: 0
-            };
-        }
+});
+
+selectorMateria2.addEventListener('change', (event) => {
+    if (event.target.value === "si") {
+        inputNota2.disabled = false;
+    } else {
+        inputNota2.disabled = true;
+        inputNota2.value = '';
     }
-}
+});
 
-//Ingreso de materias
+selectorMateria3.addEventListener('change', (event) => {
+    if (event.target.value === "si") {
+        inputNota3.disabled = false;
+    } else {
+        inputNota3.disabled = true;
+        inputNota3.value = '';
+    }
+});
 
-const materias = [
-    new Materia("Constitucional", 1234, "Mañana"),
-    new Materia("Derechos Humanos", 5678, "Tarde"),
-    new Materia("Contratos", 9123, "Noche")
+// Evento "submit" en el primer formulario
+formMaterias.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    formMaterias.style.display = 'none'; 
+    formCursada.style.display = 'block';
+});
+
+// Evento "submit" en el segundo formulario
+formCursada.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    // Calcular el promedio
+    let cantidadMaterias = 0;
+    let sumaNotas = 0;
+
+    // Verificar cuántas materias se han cursado y sumar las notas correspondientes
+    if (materia1.value === "si") {
+        cantidadMaterias++;
+        sumaNotas += parseInt(nota1.value);
+    }
+
+    if (materia2.value === "si") {
+        cantidadMaterias++;
+        sumaNotas += parseInt(nota2.value);
+    }
+
+    if (materia3.value === "si") {
+        cantidadMaterias++;
+        sumaNotas += parseInt(nota3.value);
+    }
+
+    // Calcular el promedio
+    const promedioGeneral = sumaNotas / cantidadMaterias;
+
+    // Mostrar el mensaje con el promedio
+    document.getElementById("mensaje-promedio").textContent = `Tu promedio es ${promedioGeneral.toFixed(2)}`;
+
+    // Obtener los valores de diasElegidos y horarioElegido
+    const diasElegidos = document.getElementById("dias-disponibles").value;
+    const horarioElegido = document.getElementById("horario-disponible").value;
+
+    // Guardar datos en localStorage
+    const materiaRec = recomendarMateria(diasElegidos, horarioElegido);
+    guardarDatosLocalStorage(cantidadMaterias, sumaNotas, promedioGeneral, materiaRec, diasElegidos, horarioElegido);
+
+    formCursada.style.display = 'none'; 
+    resultados.style.display = 'block'; 
+});
+
+// Materias para inscripcion
+const materiasDisponibles = [
+    { nombre: "Teoria del delito", dias: "Martes y viernes", codigo: 989898, horario: "Mañana" },
+    { nombre: "Sociedades", dias: "Martes y viernes", codigo: 878787, horario: "Tarde" },
+    { nombre: "Concursos y quiebras", dias: "Martes y viernes", codigo: 656565, horario: "Noche" },
+    { nombre: "Criminologia", dias: "sabado", codigo: 434343, horario: "Mañana" },
+    { nombre: "Sociologia", dias: "sabado", codigo: 323232, horario: "Tarde" },
+    { nombre: "Derecho Penal", dias: "Lunes y jueves", codigo: 172634, horario: "Mañana" },
+    { nombre: "Ingles", dias: "Lunes y jueves", codigo: 451289, horario: "Tarde" },
+    { nombre: "Derecho Civil", dias: "Lunes y jueves", codigo: 569812, horario: "Noche" }
 ];
 
-//Declaracion de variables sumNotas y cantAprobadas
-let sumNotas = 0;
-let cantAprobadas = 0;
+document.getElementById("form-cursada").addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const diasElegidos = document.getElementById("dias-disponibles").value;
+    const horarioElegido = document.getElementById("horario-disponible").value;
+    const materiaRecomendada = recomendarMateria(diasElegidos, horarioElegido);
 
-//Consulta de materias cursadas y notas (FOR...OF)
-for(let materia of materias){
-    let notaMateria = materia.preguntarPorMateria();
-    if (notaMateria.aprobado){
-        sumNotas += notaMateria.nota;
-        cantAprobadas++;
-    }
-}
+    document.getElementById("materia-recomendada").innerText = materiaRecomendada.nombre;
+    document.getElementById("resultados").style.display = "block";
+});
 
-// Función para validar la nota y definir si la materia está aprobada
-function validarNota(materia) {
-    if (materia < 4) {
-        alert("No aprobaste la materia. La nota mínima para aprobar una materia es de 4");
-        return false;
-    } else if (materia >= 4 && materia <= 7) {
-        alert("Aprobaste la materia!");
-        return true;
-    } else if (materia >= 8 && materia <= 10) {
-        alert("Felicitaciones!");
-        return true;
-    } else {
-        alert("La nota que ingresaste no es válida");
-        return false;
-    }
-}
+function recomendarMateria(diasElegidos, horarioElegido) {
+    const diasMap = {
+        "1": "Lunes y jueves",
+        "2": "Martes y viernes",
+        "3": "sabado"
+    };
 
+    const horarioMap = {
+        "1": "Mañana",
+        "2": "Tarde",
+        "3": "Noche"
+    };
 
-//Calculo del promedio general
-let promedioFinal = sumNotas / cantAprobadas;
-alert("Tu promedio general es: " + promedioFinal);
+    const diasSeleccionados = diasMap[diasElegidos];
+    const horarioSeleccionado = horarioMap[horarioElegido];
 
-if (promedioFinal >= 8) {
-    alert('Tu promedio es alto, felicitaciones!');
-} else if(promedioFinal <8 && promedioFinal>=6){
-    alert("Tu promedio es muy bueno!")
-} else{
-    alert('Tu promedio es algo bajo. Continuemos con la inscripción');
-}
+    const materiaFiltrada = materiasDisponibles.filter(materia =>
+        materia.dias === diasSeleccionados && materia.horario === horarioSeleccionado
+    );
 
+    return materiaFiltrada[0]; // Devuelve la primera materia que cumple con los criterios de selección
+};
 
-//Selección de días y horarios para la cursada
+function guardarDatosLocalStorage(cantidadMaterias, sumaNotas, promedioGeneral, materiaRec, diasElegidos, horarioElegido) {
+    const datos = {
+        cantidadMaterias,
+        sumaNotas,
+        promedioGeneral,
+        materiaRec,
+        diasElegidos,
+        horarioElegido
+    };
 
-alert("¿Que dias queres cursar?");
-let diasDisponibles = prompt("\n1: lunes y jueves. \n2: martes y viernes. \n3: Sabados")
-
-
-if (diasDisponibles == 1) {
-    alert("Elegiste cursar lunes y jueves");
-    alert("¿En que horarios podes cursar?");
-    let horarioDisponible = prompt("\n1: Mañana \n2: Tarde \n3: Noche")
-
-    switch (horarioDisponible) {
-        case '1':
-            alert("Elegiste turno mañana. Te recomendamos inscribirte en Derecho Penal");
-            break;
-        case '2':
-            alert("Elegiste turno tarde. Te recomendamos inscribirte en Ingles");
-            break;
-        case '3':
-            alert("Elegiste turno noche. Te recomendamos inscribirte en Derecho Civil");
-            break;
-        default:
-            alert("Opcion de horario no válida");
-    }
-
-} else if (diasDisponibles == 2) {
-    alert("Elegiste cursar martes y viernes");
-    alert("¿En que horarios podes cursar?");
-    let horarioDisponible = prompt("\n1: Mañana \n2: Tarde \n3: Noche")
-
-    switch (horarioDisponible) {
-        case '1':
-            alert("Elegiste turno mañana. Te recomendamos inscribirte en Teoria del Delito");
-            break;
-        case '2':
-            alert("Elegiste turno tarde. Te recomendamos inscribirte en Sociedades");
-            break;
-        case '3':
-            alert("Elegiste turno noche. Te recomendamos inscribirte en Concursos y Quiebras");
-            break;
-        default:
-            alert("Opcion de horario no válida");
-    }
-
-} else if (diasDisponibles == 3) {
-    alert("Elegiste cursar los sabados");
-    alert("¿En que horarios podes cursar?");
-    let horarioDisponible = prompt("\n1: Mañana. \n2: Tarde")
-
-    switch (horarioDisponible) {
-        case '1':
-            alert("Elegiste turno mañana. Te recomendamos inscribirte en Criminologia");
-            break;
-        case '2':
-            alert("Elegiste turno tarde. Te recomendamos inscribirte en Sociologia");
-            break;
-        default:
-            alert("Opcion de horario no válida");
-    }
-
-} else {
-    alert("Opcion de dias no válida");
-}
-
-//Materias para inscripcion
-const materiasDisponibles = [
-    {nombre: "Teoria del delito", dias: "Martes y viernes", codigo: 989898, horario: "Mañana"},
-    {nombre: "Sociedades", dias: "Martes y viernes", codigo: 878787, horario: "Tarde"},
-    {nombre: "Concursos y quiebras", dias: "Martes y viernes", codigo: 656565, horario: "Noche"},
-    {nombre: "Criminologia", dias: "sabado", codigo: 434343, horario: "Mañana"},
-    {nombre: "Sociologia", dias: "sabado", codigo: 323232, horario: "Tarde"},
-    {nombre: "Derecho Penal", dias: "Lunes y jueves", codigo: 172634, horario: "Mañana"},
-    {nombre: "Ingles", dias: "Lunes y jueves", codigo: 451289, horario: "Tarde"},
-    {nombre: "Derecho Civil", dias: "Lunes y jueves", codigo: 569812, horario:"Noche"}
-]
-
-//Filtro de materias por la mañana
-const materiasPorLaManana = materiasDisponibles.filter(materia => materia.horario === "Mañana");
-console.log(materiasPorLaManana);
-
-
-//Filtro de materias de martes y viernes
-const materiasMartesyViernes = materiasDisponibles.filter(materia => materia.dias === "Martes y viernes");
-console.log(materiasMartesyViernes);
-
-//Busqueda de materia por el usuario para ver sus propiedades
-const terminoDeBusqueda = "Derecho"; // término a ingresar por el usuario
-const resultadosDeBusqueda = materiasDisponibles.filter(materia => materia.nombre.toLowerCase().includes(terminoDeBusqueda.toLowerCase()));
-console.log(resultadosDeBusqueda);
+    localStorage.setItem('datosAlumno', JSON.stringify(datos));
+};
