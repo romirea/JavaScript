fd9c0af4f59700be579be8bf02ed5f27b8fa3c12
rominarea <rominarea@gmail.com>
2023-05-10 17:11:44 -0300

    solucion filtro

--- config.js
+++ config.js
@@ -94,6 +94,13 @@
         // Obtener los valores de diasElegidos y horarioElegido
         const diasElegidos = document.getElementById("dias-disponibles").value;
         const horarioElegido = document.getElementById("horario-disponible").value;
+        document.getElementById("dias-disponibles").addEventListener("change", () => {
+            actualizarTablaMaterias();
+        });
+        
+        document.getElementById("horario-disponible").addEventListener("change", () => {
+            actualizarTablaMaterias();
+        });
 
     });
 
@@ -122,8 +129,12 @@
 
     // -----------------------------------------------------
 
+//AGREGADO DE DEFINICION DE VARIABLE LISTADOMATERIAS
+    let listadoMaterias = [];
+    
 
-//OBTENER JSON
+
+    //OBTENER JSON
 
     function obtenerJSON() {
         const URLJSON = "materias.json";
@@ -131,7 +142,8 @@
             .then((respuesta) => respuesta.json())
             .then((datos) => {
                 console.log(datos);
-                let listadoMaterias = datos.materiasDisponibles;
+                listadoMaterias = datos.materiasDisponibles;
+                actualizarTablaMaterias();
 
                 listadoMaterias.forEach((materia) => {
                     let botonSeleccionar = document.createElement("button");
@@ -169,82 +181,140 @@
 
     obtenerJSON();
 
-// Lista de materias seleccionadas
-    let materiasSeleccionadas = [];
+    // -----------------------------------------------------
+// NUEVO AGREGADO
 
-// Limpiar selección
-document.getElementById("limpiar-seleccion").addEventListener("click", function () {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: 'Estás por cancelar tu inscripción. ¿Deseas continuar?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#d33',
-      confirmButtonText: 'Sí, cancelar',
-      cancelButtonText: 'No, volver'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        materiasSeleccionadas.length = 0;
-        actualizarMateriasSeleccionadas();
-      }
-    });
-  });
+function actualizarTablaMaterias() {
+    // Obtener los valores seleccionados
+    const diasSeleccionados = document.getElementById("dias-disponibles").value;
+    const horariosSeleccionados = document.getElementById("horario-disponible").value;
   
-
-  // Actualizar la tabla de materias seleccionadas
-function actualizarMateriasSeleccionadas() {
-    // Obtener la tabla
-    const tablaMateriasSeleccionadas = document.getElementById("tabla-materias-seleccionadas"); 
+    // Filtrar la lista de materias disponibles para mostrar únicamente aquellas que coinciden con los valores seleccionados
+    const materiasFiltradas = listadoMaterias.filter(materia => materia.dias.includes(diasSeleccionados) && materia.horario === horariosSeleccionados);
+  
     // Vaciar la tabla
-    tablaMateriasSeleccionadas.innerHTML = "";
+    const tablaMateriasDisponibles = document.getElementById("tabla-materias");
+    tablaMateriasDisponibles.innerHTML = "";
+  
+    // Mostrar las materias filtradas en la tabla
+    materiasFiltradas.forEach((materia) => {
+      // Crear el botón de selección de la materia
+      let botonSeleccionar = document.createElement("button");
+      botonSeleccionar.setAttribute("id", `boton-${materia.codigo}`);
+      botonSeleccionar.setAttribute("data-seleccionada", "false");
+      botonSeleccionar.textContent = "Seleccionar";
+      botonSeleccionar.addEventListener("click", () => {
+        let materiaSeleccionada = {
+          nombre: materia.nombre,
+          codigo: materia.codigo,
+          dias: materia.dias,
+          horario: materia.horario
+        };
+        agregarMateriaSeleccionada(materiaSeleccionada);
+      });
+  
+      // Crear la fila de la tabla
+      let fila = document.createElement("tr");
+      fila.innerHTML = `
+        <td>${materia.nombre}</td>
+        <td>${materia.codigo}</td>
+        <td>${materia.dias}</td>
+        <td>${materia.horario}</td>
+        <td></td>
+      `;
+      let celdaBoton = fila.querySelector("td:last-child");
+      celdaBoton.appendChild(botonSeleccionar);
+  
+      tablaMateriasDisponibles.appendChild(fila);
+    });
   }
 
-
-// Agregar materia seleccionada a la lista
-function agregarMateriaSeleccionada(materia) {
-    materiasSeleccionadas.push(materia);
-
-    // Crear fila para mostrar la materia seleccionada
-    let fila = document.createElement("tr");
-
-    // Crear celda para el nombre de la materia
-    let celdaNombre = document.createElement("td");
-    celdaNombre.textContent = materia.nombre;
-    fila.appendChild(celdaNombre);
-
-    // Crear celda para el botón "eliminar"
-    let celdaEliminar = document.createElement("td");
-    let botonEliminar = document.createElement("button");
-    botonEliminar.textContent = "Eliminar";
-    botonEliminar.addEventListener("click", () => {
-        eliminarMateriaSeleccionada(materia);
-        fila.remove();
-    });
-    celdaEliminar.appendChild(botonEliminar);
-    fila.appendChild(celdaEliminar);
-
-    // Agregar fila a la tabla de materias seleccionadas
-    let tabla = document.getElementById("tabla-materias-seleccionadas");
-    tabla.appendChild(fila);
-}
-
-// Eliminar materia seleccionada de la lista
-function eliminarMateriaSeleccionada(materia) {
-    let indice = materiasSeleccionadas.indexOf(materia);
-    if (indice !== -1) {
-        materiasSeleccionadas.splice(indice, 1);
-    }
-}
-
-//CONFIRMAR SELECCION
-const confirmarSeleccionBtn = document.getElementById('confirmar-seleccion');
-confirmarSeleccionBtn.addEventListener('click', () => {
-  Swal.fire({
-    title: '¡Inscripción enviada!',
-    text: 'Tu inscripción ha sido enviada correctamente.',
-    icon: 'success'
-  }).then(() => {
-    // Limpiar la tabla de materias seleccionadas
-    actualizarMateriasSeleccionadas();
-  });
+  // Evento "submit" en el formulario de seleccion de horarios y dias
+formCursada.addEventListener('submit', (event) => {
+    event.preventDefault();
+    formMaterias.style.display = 'none';
+    formCursada.style.display = 'block';
+    // actualizarTablaMaterias();
 });
+// ---------------------------------  
+
+
+    // Lista de materias seleccionadas
+    let materiasSeleccionadas = [];
+
+    // Limpiar selección
+    document.getElementById("limpiar-seleccion").addEventListener("click", function () {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: 'Estás por cancelar tu inscripción. ¿Deseas continuar?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'Sí, cancelar',
+            cancelButtonText: 'No, volver'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                materiasSeleccionadas.length = 0;
+                actualizarMateriasSeleccionadas();
+            }
+        });
+    });
+
+
+    // Actualizar la tabla de materias seleccionadas
+    function actualizarMateriasSeleccionadas() {
+        // Obtener la tabla
+        const tablaMateriasSeleccionadas = document.getElementById("tabla-materias-seleccionadas");
+        // Vaciar la tabla
+        tablaMateriasSeleccionadas.innerHTML = "";
+    }
+
+
+    // Agregar materia seleccionada a la lista
+    function agregarMateriaSeleccionada(materia) {
+        materiasSeleccionadas.push(materia);
+
+        // Crear fila para mostrar la materia seleccionada
+        let fila = document.createElement("tr");
+
+        // Crear celda para el nombre de la materia
+        let celdaNombre = document.createElement("td");
+        celdaNombre.textContent = materia.nombre;
+        fila.appendChild(celdaNombre);
+
+        // Crear celda para el botón "eliminar"
+        let celdaEliminar = document.createElement("td");
+        let botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.addEventListener("click", () => {
+            eliminarMateriaSeleccionada(materia);
+            fila.remove();
+        });
+        celdaEliminar.appendChild(botonEliminar);
+        fila.appendChild(celdaEliminar);
+
+        // Agregar fila a la tabla de materias seleccionadas
+        let tabla = document.getElementById("tabla-materias-seleccionadas");
+        tabla.appendChild(fila);
+    }
+
+    // Eliminar materia seleccionada de la lista
+    function eliminarMateriaSeleccionada(materia) {
+        let indice = materiasSeleccionadas.indexOf(materia);
+        if (indice !== -1) {
+            materiasSeleccionadas.splice(indice, 1);
+        }
+    }
+
+    //CONFIRMAR SELECCION
+    const confirmarSeleccionBtn = document.getElementById('confirmar-seleccion');
+    confirmarSeleccionBtn.addEventListener('click', () => {
+        Swal.fire({
+            title: '¡Inscripción enviada!',
+            text: 'Tu inscripción ha sido enviada correctamente.',
+            icon: 'success'
+        }).then(() => {
+            // Limpiar la tabla de materias seleccionadas
+            actualizarMateriasSeleccionadas();
+        });
+    });
