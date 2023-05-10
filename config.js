9636fe4f0f0216aafa342e0a935f3050409d4713
rominarea <rominarea@gmail.com>
2023-05-10 19:18:47 -0300

    modificaciones de orden

--- config.js
+++ config.js
@@ -2,16 +2,16 @@
 
     document.getElementById("btn-bienvenida").addEventListener("click", function () {
         let nombreUsuario = document.getElementById("nombre").value;
-      
-        swal.fire({
-          title: `¡Bienvenido/a ${nombreUsuario}!`,
-          text: "¡Comencemos con tu inscripción!",
-          icon: "success",
-          button: "Aceptar",
-        });
-      });
 
-      
+        swal.fire({
+            title: `¡Bienvenido/a ${nombreUsuario}!`,
+            text: "¡Comencemos con tu inscripción!",
+            icon: "success",
+            button: "Aceptar",
+        });
+    });
+
+
     // Selección de elementos del DOM
     const selectorMateria1 = document.getElementById('materia1');
     const selectorMateria2 = document.getElementById('materia2');
@@ -74,7 +74,7 @@
         let cantidadMaterias = 0;
         let sumaNotas = 0;
 
-        // Verificar cuántas materias se han cursado y sumar las notas correspondientes
+        // Verificar cuántas materias se cursaron y sumar las notas correspondientes
         if (selectorMateria1.value === "si") {
             cantidadMaterias++;
             sumaNotas += parseInt(inputNota1.value);
@@ -102,7 +102,7 @@
         document.getElementById("dias-disponibles").addEventListener("change", () => {
             actualizarTablaMaterias();
         });
-        
+
         document.getElementById("horario-disponible").addEventListener("change", () => {
             actualizarTablaMaterias();
         });
@@ -134,9 +134,8 @@
 
     // -----------------------------------------------------
 
-//AGREGADO DE DEFINICION DE VARIABLE LISTADOMATERIAS
+    //AGREGADO DE DEFINICION DE VARIABLE LISTADOMATERIAS
     let listadoMaterias = [];
-    
 
 
     //OBTENER JSON
@@ -187,61 +186,60 @@
     obtenerJSON();
 
     // -----------------------------------------------------
-// NUEVO AGREGADO
 
-function actualizarTablaMaterias() {
-    // Obtener los valores seleccionados
-    const diasSeleccionados = document.getElementById("dias-disponibles").value;
-    const horariosSeleccionados = document.getElementById("horario-disponible").value;
-  
-    // Filtrar la lista de materias disponibles para mostrar únicamente aquellas que coinciden con los valores seleccionados
-    const materiasFiltradas = listadoMaterias.filter(materia => materia.dias.includes(diasSeleccionados) && materia.horario === horariosSeleccionados);
-  
-    // Vaciar la tabla
-    const tablaMateriasDisponibles = document.getElementById("tabla-materias");
-    tablaMateriasDisponibles.innerHTML = "";
-  
-    // Mostrar las materias filtradas en la tabla
-    materiasFiltradas.forEach((materia) => {
-      // Crear el botón de selección de la materia
-      let botonSeleccionar = document.createElement("button");
-      botonSeleccionar.setAttribute("id", `boton-${materia.codigo}`);
-      botonSeleccionar.setAttribute("data-seleccionada", "false");
-      botonSeleccionar.textContent = "Seleccionar";
-      botonSeleccionar.addEventListener("click", () => {
-        let materiaSeleccionada = {
-          nombre: materia.nombre,
-          codigo: materia.codigo,
-          dias: materia.dias,
-          horario: materia.horario
-        };
-        agregarMateriaSeleccionada(materiaSeleccionada);
-      });
-  
-      // Crear la fila de la tabla
-      let fila = document.createElement("tr");
-      fila.innerHTML = `
+    function actualizarTablaMaterias() {
+        // Obtener los valores seleccionados
+        const diasSeleccionados = document.getElementById("dias-disponibles").value;
+        const horariosSeleccionados = document.getElementById("horario-disponible").value;
+
+        // Filtrar la lista de materias disponibles para mostrar únicamente aquellas que coinciden con los valores seleccionados
+        const materiasFiltradas = listadoMaterias.filter(materia => materia.dias.includes(diasSeleccionados) && materia.horario === horariosSeleccionados);
+
+        // Vaciar la tabla
+        const tablaMateriasDisponibles = document.getElementById("tabla-materias");
+        tablaMateriasDisponibles.innerHTML = "";
+
+        // Mostrar las materias filtradas en la tabla
+        materiasFiltradas.forEach((materia) => {
+            // Crear el botón de selección de la materia
+            let botonSeleccionar = document.createElement("button");
+            botonSeleccionar.setAttribute("id", `boton-${materia.codigo}`);
+            botonSeleccionar.setAttribute("data-seleccionada", "false");
+            botonSeleccionar.textContent = "Seleccionar";
+            botonSeleccionar.addEventListener("click", () => {
+                let materiaSeleccionada = {
+                    nombre: materia.nombre,
+                    codigo: materia.codigo,
+                    dias: materia.dias,
+                    horario: materia.horario
+                };
+                agregarMateriaSeleccionada(materiaSeleccionada);
+            });
+
+            // Crear la fila de la tabla
+            let fila = document.createElement("tr");
+            fila.innerHTML = `
         <td>${materia.nombre}</td>
         <td>${materia.codigo}</td>
         <td>${materia.dias}</td>
         <td>${materia.horario}</td>
         <td></td>
-      `;
-      let celdaBoton = fila.querySelector("td:last-child");
-      celdaBoton.appendChild(botonSeleccionar);
-  
-      tablaMateriasDisponibles.appendChild(fila);
-    });
-  }
+    `;
+            let celdaBoton = fila.querySelector("td:last-child");
+            celdaBoton.appendChild(botonSeleccionar);
 
-  // Evento "submit" en el formulario de seleccion de horarios y dias
-formCursada.addEventListener('submit', (event) => {
-    event.preventDefault();
-    formMaterias.style.display = 'none';
-    formCursada.style.display = 'block';
-    // actualizarTablaMaterias();
-});
-// ---------------------------------  
+            tablaMateriasDisponibles.appendChild(fila);
+        });
+    }
+
+    // Evento "submit" en el formulario de seleccion de horarios y dias
+    formCursada.addEventListener('submit', (event) => {
+        event.preventDefault();
+        formMaterias.style.display = 'none';
+        formCursada.style.display = 'block';
+        actualizarTablaMaterias();
+    });
+    // ---------------------------------  
 
 
     // Lista de materias seleccionadas
